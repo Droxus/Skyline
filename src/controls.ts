ced206990d20adfbb9f539061da7c5be6f27f672
Droxus <alexredov@gmail.com>
2023-02-15 20:13:46 +0200

    +three js; +cube

---
+++ src/controls.ts
@@ -0,0 +1,11 @@
+import * as THREE from 'three';
+import * as main from './main'
+
+export let cube: any
+
+export function createCube(){
+  cube = new THREE.Mesh( new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'green'}))
+  main.scene.add( cube );
+  cube.position.set(0, 0, -5)
+  cube.rotation.set(0, Math.PI/4, 0)
+}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,41 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import * as Controls from './controls'
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+import * as THREE from 'three';
+
+export const scene = new THREE.Scene();
+
+export const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+export const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+Controls.createCube()
+
+animate();
+function animate() {
+    requestAnimationFrame(animate)
+
+    // camera.position.x = playerModel.position.x
+    // camera.position.y = playerModel.position.y + playerModel.geometry.parameters.height/2 * playerModel.scale.y
+    // camera.position.z = playerModel.position.z
+
+    Controls.cube.rotation.x += 0.01;
+    Controls.cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+};
+
+window.addEventListener('resize', onResize)
+document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
+window.addEventListener('beforeunload', function(e){
+  e.stopPropagation();e.preventDefault();return false;
+},true);
+
+function onResize(){
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
